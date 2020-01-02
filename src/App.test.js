20f463e4f38edd3d6d34299751a8591026c425fd
Aurelien Brachet <aurelienbrachet@macbook-pro-de-aurelien.home>
2020-01-02 15:49:48 +0200

    started project

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("render without errors", () => {
+  const wrapper = shallow(<App />);
+  const component = wrapper.find("[data-test='component-app ']");
+  expect(component.length).toBe(1);
 });
+
+test("render increment button", () => {});
+
+test("render display counter", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clicking the button increments the counter", () => {});
